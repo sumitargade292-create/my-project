607f0bcb6c76cec253f0f8742e5c05dd147b8c3d
Sumit Aragade <sumitaragade292@gmail.com>
2025-10-01 15:47:30 +0530

    Fixed import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import  { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
-import EmployeeDashboard from './components/Dashboards/EmployeeDashboard'
+import EmployeeDashboard from './Components/Dashboards/EmployeeDashboard'
 import AdminDashboard from './components/Dashboards/AdminDashboard'
 import { AuthContext } from './context/AuthProvider'
 
